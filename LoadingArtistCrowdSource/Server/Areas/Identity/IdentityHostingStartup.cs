08a803d2e081dfef58557a5885e427667396f019
James Linnell <jman012guy@gmail.com>
2021-01-09 14:10:50 -0800

    Identity scaffolding

---
+++ LoadingArtistCrowdSource/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LoadingArtistCrowdSource.Server.Data;
+using LoadingArtistCrowdSource.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LoadingArtistCrowdSource.Server.Areas.Identity.IdentityHostingStartup))]
+namespace LoadingArtistCrowdSource.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
