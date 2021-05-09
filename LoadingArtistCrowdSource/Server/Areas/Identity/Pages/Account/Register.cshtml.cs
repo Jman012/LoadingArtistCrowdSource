d1ee0ba4321f814d8e79470a349638c2bc0662f6
James Linnell <jman012guy@gmail.com>
2021-05-09 10:10:21 -0700

    Change how filter for textfield works. Don't track URLs in emails (thanks, MailJet).

--- LoadingArtistCrowdSource/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LoadingArtistCrowdSource/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,7 +107,7 @@ namespace LoadingArtistCrowdSource.Server.Areas.Identity.Pages.Account
 						protocol: Request.Scheme);
 
 					await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-						$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+						$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}' rel='notrack'>clicking here</a>.");
 
 					if (_userManager.Options.SignIn.RequireConfirmedAccount)
 					{
