170e86deec2e5026c243eb3fe8133f1bd80525fb
James Linnell <jman012guy@gmail.com>
2021-05-12 07:58:53 -0700

    Enable HSTS.

--- LoadingArtistCrowdSource/Server/Startup.cs
+++ LoadingArtistCrowdSource/Server/Startup.cs
@@ -159,7 +159,7 @@ namespace LoadingArtistCrowdSource.Server
 			{
 				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				// app.UseHsts();
+				app.UseHsts();
 			}
 
 			app.UseHttpsRedirection();
